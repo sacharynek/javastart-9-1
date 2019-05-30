71d4d9670bd6b43c4f85a915d8615b576dfcc724
tomaszlach <tomaszlach86@gmail.com>
2019-05-30 18:21:28 +0200

    added rangom number generator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        int[] tab = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] tab = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12};
         int counter = 0;
+        Random rand = new Random();
+        for(int i = 0; i<tab.length; i++){
+            tab[i] = rand.nextInt(100);
+        }
 
         for (int i = 0; i < (2 * tab.length); i++) {
 
