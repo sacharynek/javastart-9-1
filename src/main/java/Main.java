e857d7e63e23413cb2a85cc27c21161093011cf9
tomaszlach <tomaszlach86@gmail.com>
2019-05-30 11:33:41 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        int[] tab = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int counter = 0;
+
+        for (int i = 0; i < (2 * tab.length); i++) {
+
+            //System.out.println("counter: " + counter);
+            System.out.print(tab[counter]+" ");
+            if (i < tab.length) {
+                counter++;
+                if (counter == tab.length) {
+                    counter--;
+                }
+
+            } else {
+                counter--;
+            }
+
+        }
+    }
+}
