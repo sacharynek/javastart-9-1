89ce3c4950b5c73d8d8327fc6e93196ac21b6955
tomaszlach <tomaszlach86@gmail.com>
2019-05-31 09:51:00 +0200

    final polish

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,10 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        int[] tab = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12};
+        int[] tab = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
         int counter = 0;
         Random rand = new Random();
-        for(int i = 0; i<tab.length; i++){
+        for (int i = 0; i < tab.length; i++) {
             tab[i] = rand.nextInt(100);
         }
 
